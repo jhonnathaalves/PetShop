0339b65ff8e04e72832e68b26a0fba444d20e31f
unknown <jhonnatha16011987@yahoo.com.br>
2021-11-01 22:32:41 -0300

    Pessoa, Cliente e Funcionario, Telefones e Endereços, criado relacionamento

--- src/main/java/com/jhonnatha/petshop/utils/PopulaDados.java
+++ src/main/java/com/jhonnatha/petshop/utils/PopulaDados.java
@@ -9,15 +9,20 @@ import org.springframework.stereotype.Component;
 
 import com.jhonnatha.petshop.domain.Categoria;
 import com.jhonnatha.petshop.domain.Cidade;
+import com.jhonnatha.petshop.domain.Cliente;
+import com.jhonnatha.petshop.domain.Endereco;
 import com.jhonnatha.petshop.domain.Especie;
 import com.jhonnatha.petshop.domain.Estado;
+import com.jhonnatha.petshop.domain.Funcionario;
 import com.jhonnatha.petshop.domain.Pet;
 import com.jhonnatha.petshop.domain.Produto;
 import com.jhonnatha.petshop.domain.Raca;
 import com.jhonnatha.petshop.repository.CategoriaRepository;
 import com.jhonnatha.petshop.repository.CidadeRepository;
+import com.jhonnatha.petshop.repository.EnderecoRepository;
 import com.jhonnatha.petshop.repository.EspecieRepository;
 import com.jhonnatha.petshop.repository.EstadoRepository;
+import com.jhonnatha.petshop.repository.PessoaRepository;
 import com.jhonnatha.petshop.repository.PetRepository;
 import com.jhonnatha.petshop.repository.ProdutoRepository;
 import com.jhonnatha.petshop.repository.RacaRepository;
@@ -45,6 +50,12 @@ public class PopulaDados {
 
 	@Autowired
 	CidadeRepository cidadeRepository;
+	
+	@Autowired
+	PessoaRepository pessoaRepository;
+
+	@Autowired
+	EnderecoRepository enderecoRepository;
 
 	@PostConstruct
 	public void cadastrar() {
@@ -99,6 +110,20 @@ public class PopulaDados {
 
 		estadoRepository.saveAll(Arrays.asList(est1, est2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+		
+		Cliente clt1 = new Cliente(null, "Jose Maria", "jose@mail.com", "335.194.320-21", "FISICA");
+		clt1.getTelefones().addAll(Arrays.asList("3516-2000","9191-0000"));
+
+		Funcionario fnc1 = new Funcionario(null, "Maria Jose", "maria@mail.com", "551.872.200.12", "ATENDENTE");
+		fnc1.getTelefones().addAll(Arrays.asList("3279-0001","9090-0002"));
+
+
+		Endereco end1 = new Endereco(null, "Rua Tupis", "500", "Apto 101", "Pindorama", "30111222", clt1, c1);
+		Endereco end2 = new Endereco(null, "Av. Tamoios", "100", "Casa", "Oca", "3968000", fnc1, c2);
+		Endereco end3 = new Endereco(null, "Rua Aranãs", "10", "Apto 201", "Centro", "01153000", fnc1, c3);
+
+		pessoaRepository.saveAll(Arrays.asList(clt1, fnc1));
+		enderecoRepository.saveAll(Arrays.asList(end1, end2, end3));
 
 	}
 
