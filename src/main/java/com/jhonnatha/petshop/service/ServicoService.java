71f4f8cd31bbecef926cdfee282425b51e575ca9
unknown <jhonnatha16011987@yahoo.com.br>
2021-11-01 23:48:01 -0300

    Criando o Post, delete e Put de pessoas e servico. E listando Pessoas com DTO

--- src/main/java/com/jhonnatha/petshop/service/ServicoService.java
+++ src/main/java/com/jhonnatha/petshop/service/ServicoService.java
@@ -3,10 +3,12 @@ package com.jhonnatha.petshop.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.jhonnatha.petshop.domain.Servico;
 import com.jhonnatha.petshop.repository.ServicoRepository;
+import com.jhonnatha.petshop.resources.exceptions.DataIntegrityException;
 import com.jhonnatha.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @Service
@@ -18,6 +20,26 @@ public class ServicoService {
 	public Servico find(Integer id) {
 		Optional<Servico> obj = repo.findById(id);
 		return obj.orElseThrow( () -> new ObjetoNaoEncontradoException( "Objeto não encontrado. ID: " + id + ", Tipo: " + Servico.class.getName()));
+	}
+	
+	public Servico insert(Servico obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+
+	public Servico update(Servico obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}	
+
+	public void delete(Integer id) {
+		find(id);		
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Servico possui produtos, não é possível deletar!");
+		}
+
 	}
 
 }
