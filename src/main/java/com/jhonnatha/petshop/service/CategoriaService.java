570d5d22e966e494ad36d3649b6bf001877e14f5
unknown <jhonnatha16011987@yahoo.com.br>
2021-11-01 21:45:36 -0300

    Tratando Exceções e Referências Cíclicas

--- src/main/java/com/jhonnatha/petshop/service/CategoriaService.java
+++ src/main/java/com/jhonnatha/petshop/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jhonnatha.petshop.domain.Categoria;
 import com.jhonnatha.petshop.repository.CategoriaRepository;
+import com.jhonnatha.petshop.service.exceptions.ObjetoNaoEncontradoException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,7 @@ public class CategoriaService {
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow( () -> new ObjetoNaoEncontradoException( "Objeto não encontrado. ID: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
